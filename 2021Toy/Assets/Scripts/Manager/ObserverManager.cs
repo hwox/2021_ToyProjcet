3cd30670e3e09af45247368337c47b428a907889
혜원 <v_vpollo@naver.com>
2021-09-10 00:59:33 +0900

    [추가] 옵저버 매니저 추가
    HashTable로 param을 넘겨 받고 싶어서
    
    (cherry picked from commit 9e80870bc09ade8f986862747855efdb3d6e8c87)

---
+++ 2021Toy/Assets/Scripts/Manager/ObserverManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface IHashRecv
+{
+    void receiveCall(string key, Hashtable param);
+}
+
+public class ObserverManager : Singleton<ObserverManager>
+{
+    Dictionary<string, List<IHashRecv>> eventHashDic = new Dictionary<string, List<IHashRecv>>();
+
+
+    public void registerEvent(string key, IHashRecv recv)
+    {
+        if(!eventHashDic.ContainsKey(key))
+        {
+            List<IHashRecv> hashList = new List<IHashRecv>();
+            eventHashDic.Add(key, hashList);
+        }
+
+        eventHashDic[key].Add(recv);
+    }
+
+
+    public void unRegisterEvent(string key, IHashRecv recv)
+    {
+        if (!eventHashDic.ContainsKey(key))
+            return;
+
+        eventHashDic[key].Remove(recv);
+    }
+
+    public void removeAllHashEvent(string key)
+    {
+        if (!eventHashDic.ContainsKey(key))
+            return;
+
+        eventHashDic[key].Clear();
+        eventHashDic.Remove(key);
+    }
+
+  
+    // 입력된 key값의 이벤트를 호출
+    public void dispatch(string key, Hashtable param)
+    {
+        if (!eventHashDic.ContainsKey(key)) return;
+
+        var handler = eventHashDic[key];
+        for (int i=0;i< handler.Count; ++i)
+        {
+            if(handler[i] != null)
+                handler[i].receiveCall(key, param);
+        }
+    }
+}
